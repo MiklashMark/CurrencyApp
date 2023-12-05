551ed7fc8d48f2b327691d6dea64d0cac3f098cd
Mark Miklash <markmiklash@gmail.com>
2023-12-05 21:31:26 +0300

    #1 Add main functional

---
+++ CurrencyApp/src/main/java/by/bsuir/markMiklash/currencyApp/Main.java
@@ -0,0 +1,17 @@
+package by.bsuir.markMiklash.currencyApp;
+
+import by.bsuir.markMiklash.currencyApp.core.dto.ExchangeRateDTO;
+import by.bsuir.markMiklash.currencyApp.dao.entity.ExchangeRateEntity;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+}
